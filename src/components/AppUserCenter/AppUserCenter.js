6364512da5783e7615daf9c3ccff2e56a8d1429b
wodewone <wodewone@sina.com>
2017-03-23 00:19:29 +0800

    update usercenter

--- src/components/AppUserCenter/AppUserCenter.js
+++ src/components/AppUserCenter/AppUserCenter.js
@@ -25,14 +25,28 @@ class AppUserCenter extends Component{
     render(){
         const USERCENTER = this.state.usercenter.user
         return(
-            <div className="scroll-content">
+            <div className="scroll-content" styleName="page-content">
                 <header styleName="center-header">
                     <div styleName="header-back" style={{backgroundImage: `url(${USERCENTER.backgroundAvatar})`}}></div>
                     <div styleName="header-inner">
                         <img styleName="user-avatar" src={USERCENTER.avatar} alt={USERCENTER.username}/>
-                        <p styleName="user-name">{USERCENTER.username}</p>
+                        <p styleName="user-info"><span styleName="user-name">{USERCENTER.username}</span> <br/> <span styleName="user-desc">{USERCENTER.bio}</span></p>
                     </div>
                 </header>
+                <section styleName="center-main">
+                    <div styleName="center-row">
+                        <button styleName="row-button">训练等级</button>
+                        <button styleName="row-button">跑步等级</button>
+                        <button styleName="row-button">骑行等级</button>
+                        <button styleName="row-button">我的徽章</button>
+                        <button styleName="row-button">我的活动</button>
+                    </div>
+                    <div styleName="center-row">
+                        <button styleName="row-button">购物车</button>
+                        <button styleName="row-button">我的订单</button>
+                        <button styleName="row-button">优惠券</button>
+                    </div>
+                </section>
             </div>
         )
     }
